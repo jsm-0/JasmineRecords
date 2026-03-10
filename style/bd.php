bd41b7f319fa4de02feaba7e50a22ea0fbd4b2c1
Jasmine <153243530+jsm-0@users.noreply.github.com>
2026-03-10 15:33:20 +0100

    remodif mdp

--- style/bd.php
+++ style/bd.php
@@ -4,7 +4,7 @@ $port = 3308;
 $host = "localhost";
 $dbname = "jasminerecordsbdd";
 $user = "jsrecords";
-$pass = "Jajajolie1!";
+$pass = "jsrecords2026";
 
 $conn = new PDO("mysql:host=$host; port=$port; dbname=$dbname", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
